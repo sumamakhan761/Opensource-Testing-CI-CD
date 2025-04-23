4f179844d6087b8ee7ac8cf5bd18aa047c1f0a66
sumamakhan7611 <khansumama87111@gmail.com>
2025-04-23 18:32:53 +0530

    update

--- src/generated/prisma/index.js
+++ src/generated/prisma/index.js
@@ -149,11 +149,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://postgres:mysecretpassword@localhost:5432/postgres"
+        "value": null
       }
     }
   },
